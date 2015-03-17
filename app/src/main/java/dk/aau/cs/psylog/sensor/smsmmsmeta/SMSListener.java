67a5badb29dbf5aeb7201d08ddc5842dfe975ce9
deaddog <mikkelslsl@gmail.com>
2015-03-17 15:28:54 +0100

    Sensor loads last read-time from existing data.

--- app/src/main/java/dk/aau/cs/psylog/sensor/smsmmsmeta/SMSListener.java
+++ app/src/main/java/dk/aau/cs/psylog/sensor/smsmmsmeta/SMSListener.java
@@ -1,23 +1,34 @@
 package dk.aau.cs.psylog.sensor.smsmmsmeta;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.telephony.SmsManager;
+import android.database.Cursor;
+import android.net.Uri;
 
 import dk.aau.cs.psylog.module_lib.DBAccessContract;
 import dk.aau.cs.psylog.module_lib.ISensor;
 
 public class SMSListener implements ISensor {
-    private SmsManager smsManager;
+    private ContentResolver resolver;
 
     public SMSListener(Context context) {
-        smsManager = SmsManager.getDefault();
+        resolver = context.getContentResolver();
     }
 
     public void startSensor() {
     }
 
     public void stopSensor() {
+    }
+
+    private void loadData() {
+        Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "smsmmsmeta");
+        Cursor cursor = resolver.query(uri, new String[]{"MAX(time)"}, null, null, null);
+
+        long last = 0;
+        if (cursor.moveToNext())
+            last = cursor.getLong(0);
     }
 
     @Override
