682cb29072c3b81e2abdb0e17fd21cfc5a132e0f
deaddog <mikkelslsl@gmail.com>
2015-03-18 10:41:56 +0100

    Created a method for loading the contents of sms inbox/sent into the sensor db.

--- app/src/main/java/dk/aau/cs/psylog/sensor/smsmmsmeta/SMSListener.java
+++ app/src/main/java/dk/aau/cs/psylog/sensor/smsmmsmeta/SMSListener.java
@@ -1,10 +1,12 @@
 package dk.aau.cs.psylog.sensor.smsmmsmeta;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.Telephony.Sms;
 
 import dk.aau.cs.psylog.module_lib.DBAccessContract;
 import dk.aau.cs.psylog.module_lib.ISensor;
@@ -35,6 +37,34 @@ public class SMSListener implements ISensor {
         long last = 0;
         if (cursor.moveToNext())
             last = cursor.getLong(0);
+
+        loadSms(last);
+    }
+
+    private void loadSms(long lastdate) {
+        String dateFilter = Sms.Inbox.DATE + " > " + String.valueOf(lastdate);
+
+        String[] inboxCols = new String[]{Sms.Inbox.ADDRESS, Sms.Inbox.BODY, Sms.Inbox.DATE};
+        Cursor inbox = resolver.query(Sms.Inbox.CONTENT_URI, inboxCols, dateFilter, null, null);
+        while (inbox.moveToNext()) {
+            ContentValues values = new ContentValues();
+            values.put(CONTACT, inbox.getString(0));
+            values.put(LENGTH, inbox.getString(1).length());
+            values.put(TIME, inbox.getLong(2));
+            values.put(INCOMING, true);
+            resolver.insert(dbUri, values);
+        }
+
+        String[] sentCols = new String[]{Sms.Inbox.ADDRESS, Sms.Inbox.BODY, Sms.Inbox.DATE_SENT};
+        Cursor outbox = resolver.query(Sms.Sent.CONTENT_URI, sentCols, dateFilter, null, null);
+        while (inbox.moveToNext()) {
+            ContentValues values = new ContentValues();
+            values.put(CONTACT, inbox.getString(0));
+            values.put(LENGTH, inbox.getString(1).length());
+            values.put(TIME, inbox.getLong(2));
+            values.put(INCOMING, false);
+            resolver.insert(dbUri, values);
+        }
     }
 
     @Override
